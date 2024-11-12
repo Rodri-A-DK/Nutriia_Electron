3c2e40e7955b497334f0db024baaf58d21237c11
unknown <rodrieldk1@gmail.com>
2024-11-12 03:19:09 -0300

    Updates

---
+++ static/js/952.3749e989.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunknutria=self.webpackChunknutria||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.3749e989.chunk.js.map
